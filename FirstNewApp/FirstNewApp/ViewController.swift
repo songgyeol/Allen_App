4a060d4ba52b8849b61048c857bd3a289b9659cd
songgyeol <songgyeol@kakao.com>
2022-02-01 21:35:38 +0900

    FirstNewApp

---
+++ FirstNewApp/FirstNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstNewApp
+//
+//  Created by 송결 on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
